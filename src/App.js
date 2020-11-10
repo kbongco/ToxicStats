32b5c9bd580a14dd64ca7840f01dcba7a49f8909
Kathleen B <kathleenb@Kathleens-MacBook-Air.local>
2020-11-09 19:50:59 -0500

    starting line graph

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import LineChart from "react-linechart";
+import "../node_modules/react-linechart/dist/styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    const data = [
+      {
+        color: "steelblue",
+        points: [
+          { x: 9 / 3, y: 1880 },
+          { x: 9 / 3, y: 1903 },
+        ],
+      },
+    ];
+    return (
+      <div>
+        <div className="App">
+          <h1>Overwatch Stats Season 12 </h1>
+          <LineChart
+            width={600}
+            height={2200}
+            data={data}/>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
